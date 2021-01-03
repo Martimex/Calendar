f68586857d52ce7452bcb09eb4bb8747afc41682
Kamil Marciniak <76244675+Martimex@users.noreply.github.com>
2021-01-03 16:31:03 +0100

    Update onload.js

--- onload.js
+++ onload.js
@@ -1,15 +1,6 @@
-const reload = document.querySelector('#month');
-
-/*
-reload.addEventListener('load', (event) => {
-   const ev = event.target;
-    ev.value = "jan";
-})
-*/
 
 const bod = document.body;
 
 bod.addEventListener('reload', () => {
-    //document.getElementById('month').options[0].selected = 'selected';
     document.querySelector('.this-month').textContent = 'January';
 })
