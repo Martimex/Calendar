f904d7e1538ddb30a7dd185894e3bc7e1b757e3e
Kamil Marciniak <76244675+Martimex@users.noreply.github.com>
2021-01-17 18:00:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,12 +20,8 @@ selectBar.addEventListener('change', (event) => {
     const ev = event.target;
     const val = ev.value;
 
-    changeMonth(val); // zmienia wyświelaną nazwę miesiąca
-    changeDaysCount(val);  // zmienia ułożenie nazw dni tygodnia
-    //changeMonthCount(val); // ile dni ma nowy miesiąc? Mniej czy Więcej a może tyle samo co poprzedni? Zmodyfikuj ilość kafelek w porównaniu z wcześniej
-    // sprawdzanym miesiącem
-
-    
+    changeMonth(val); 
+    changeDaysCount(val);    
 });
 
 
@@ -59,34 +55,20 @@ function changeDaysCount (optionval)
         MaximumYear  = year 2099;
     */
     const dateYear = document.querySelector('.this-year');
-    const minimalYear = 1920;       //minimalny rok który obsługuje ten kalendarz  // 1 stycznia tego roku to sobota
+    const minimalYear = 1920;       
     let year = dateYear.dataset.year;
     year = parseInt(year);
-    console.log('year :  '+year); 
-    console.log('minimalYear :  '+minimalYear);
 
-    let leapYear;  // warunek jeżeli mamy rok przestępny, a miesiąc docelowy to styczeń albo luty
+    let leapYear;  
     (((year%400 === 0) || ((year%4 === 0) && (year%100 !==0))) && ((optionval === 'jan') || (optionval === 'feb')))?  
     leapYear = Math.floor(((year - minimalYear)/4)-1) :  leapYear = Math.floor((year - minimalYear)/4);
 
-    console.log('Dodatkowe dni z lat przestępnych =  ' +leapYear);
     let extra = Math.floor(( 5+ (year - minimalYear) + leapYear)%7);  // 1. stycznia 2000 roku (minimalYear) to sobota, ale od dnia tygodnia,
     // teraz wtorek 1. stycznia 1924 roku (minimalYear), +7 dla soboty, a dla wtorku +3; czwartek - 1. stycznia 1920 roku (minimalYear), +5 dla czwartku
-    console.log(extra);
-
-    // jak na razie mamy wszystkie kartki z kalendarza dla stycznia danego roku
-    // to jest zgodne dla wszystkich miesięcy  po lutym (dla stycznia i lutego w roku przestępnym występują błędy) - EDIT: DZIAŁA !
 
     weekArr= ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    //const weekdays = document.querySelectorAll('.weekday');
-
-    // jeżeli sprawdzany rok jest przestępny, a user sprawdza miesiące styczeń lub luty, wtedy zacznij iterować tablicę 1 miejsce wcześniej
-    
-    // trzeba dodać dni z poprzednich miesięcy  - łączna ilość dni która minęła od 1.1 trzeba zsumować i wyciągnąć resztę z dzielenia przez 7 , a
-    // pozostały wynik dodać do zmiennej extra
-
-    let altdays = null;  // dni w roku, które już minęły w tym roku, w pętli for n
+    let altdays = null; 
 
     for(let i=0; i<=11; i++)
     {
@@ -97,27 +79,6 @@ function changeDaysCount (optionval)
 
     (extra === 0)? extra = 7 : '';
 
-    console.log('altdays =  '+altdays);
-
-    //let counter = extra; // counter służy do wyciągania stringów z tablicy zgodnie z wartością zmiennej extra
-
-   /*
-    ((year%4 === 0) && (optionval === 'jan' || optionval === 'feb'))?  // wzór na wyjątek, więc nie trzeba powyżej uwzględniać żadnych wyjątków
-    //true
-    weekdays.forEach(day => {
-        (counter === 1)? counter = 8 : '';
-        day.textContent = weekArr[counter-2];
-        (counter >=7)?  counter=0 : ''; 
-        counter++;
-    })
-    : // false
-    weekdays.forEach(day => {
-        (counter === 0)? counter = 7 : '';
-        day.textContent = weekArr[counter-1];
-        (counter >=7)?  counter=0 : ''; 
-        counter++;
-    })
-    */
     let thisMonth = null;
 
     for(let month of months)
@@ -125,38 +86,27 @@ function changeDaysCount (optionval)
         (month.val === optionval)? thisMonth = month : "";
     }
 
-   let  monthBefore = null; // po prostu miesiąc o 1 wcześniejszy od thisMonth
+   let  monthBefore = null; 
 
    (thisMonth.val === 'jan')? monthBefore = dec : '';
-
 
     for(let monthy of months)
     {
         (((thisMonth.wYD) - (monthy.wYD) >= 7))?  monthBefore = monthy : '';
-       // console.log('działanie: ')
-       // console.log((thisMonth.wYD) - (thisMonth.days-1));
-       // console.log((monthy.wYD));
     }
-
-    console.log( 'monhsj  '+monthBefore.val);
-
-    console.log('Nazwa tego miesiąca to : ' + thisMonth.name); // miesiąc na który user zmienił - mamy wszystkie dane liczbę dni :)
-    console.log(extra); // np. reszta 1 to poniedziałek, 7 to niedziela; extra oznacza ile kafelek z przodu trzeba zblankować -1
-    // Za 5 h 30 minut Nowy RoK!!
 
     const blank = document.querySelectorAll('.grid-container > div:not(.weekday)');
 
-    blank.forEach(tile => {tile.classList.remove('day'); tile.classList.add('blank');}) // resetuje klasę .day i ustawia wszystkie tile na .blank
+    blank.forEach(tile => {tile.classList.remove('day'); tile.classList.add('blank');}) 
 
-    for(let i=extra; i<=(extra+thisMonth.days)-1; i++)   // resetuje klasę .blank i dodaje do odpowiednich elementów klasę .day
+    for(let i=extra; i<=(extra+thisMonth.days)-1; i++)  
     {
-        //console.log('well...');
-        let newdays = document.querySelector(`.grid-container > div:nth-of-type(${i+7})`);
-        newdays.classList.remove('blank');
+       let newdays = document.querySelector(`.grid-container > div:nth-of-type(${i+7})`);
+       newdays.classList.remove('blank');
        newdays.classList.add('day');
        newdays.textContent = `${(i-extra)+1}`;
 
-       if((thisMonth.val === 'feb') && ((year%400 === 0) || ((year%4 === 0) && (year%100 !==0))) && (i===(extra+thisMonth.days)-1)) {  //dodajemy dodatkowy dzień gdy miesiąc to luty w roku nieparzystym  
+       if((thisMonth.val === 'feb') && ((year%400 === 0) || ((year%4 === 0) && (year%100 !==0))) && (i===(extra+thisMonth.days)-1)) {    
         i++;
         let newdays = document.querySelector(`.grid-container > div:nth-of-type(${i+7})`);
         newdays.classList.remove('blank');
@@ -165,15 +115,12 @@ function changeDaysCount (optionval)
        }
     }
 
-    // zmieniamy numerację pól blank
-
-    let blankBefore = [];  // tutaj trzeba zdobyć info o liczbie dni w poprzednim miesiącu, jeśli to styczeń to cofamy do grudnia, jeśli luty to czy rok przestępny czy nie
+    let blankBefore = [];  
     let blankAfter = []; 
     
     //blankBefore 
 
     const firstTileClass = document.querySelector('.grid-container > div:not(.weekday)');
-    console.log(firstTileClass);
 
     let counter = 1;
 
@@ -190,20 +137,16 @@ function changeDaysCount (optionval)
     
         for(let i=blankBefore.length-1; i>=0; i--)
         {
-            console.log('monthBefore.days=  ' + monthBefore.days)
             blankBefore[i].textContent = monthBefore.days - count;
             count++;
         }
-
-        console.log('okaaay');
     }
 
     else if(blankBefore.length === 1) {
         blankBefore[0].textContent = monthBefore.days;
     }
 
-    console.log(blankBefore);
-
+    
     // BlankAfter 
 
     let counting = 0;
@@ -215,13 +158,11 @@ function changeDaysCount (optionval)
         blankAfterTile = document.querySelector(`.grid-container > div:nth-last-child(${counting})`);
         blankAfter.unshift(document.querySelector(`.grid-container > div:nth-last-child(${counting})`));
     }
+    
     blankAfter.reverse();
     blankAfter.pop();
-    //console.log(blankAfter);
+
     for(let y=blankAfter.length-1; y>=0; y--) {
         blankAfter[y].textContent = `${blankAfter.length-y}`;
     }
-
-
-
 }
